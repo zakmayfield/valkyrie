2c340cc29c526a8240452e09f05bd18d6746b37c
Zak Mayfield <zakmayfield@gmail.com>
2023-04-06 12:34:31 -0400

    several updates

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,17 +2,23 @@ import * as React from 'react';
 import { Link, PageProps } from 'gatsby';
 import Layout from '../components/layout';
 import { StaticImage } from 'gatsby-plugin-image';
+import Seo from '../components/seo';
 
 const AboutPage: React.FC<PageProps> = () => {
   return (
     <Layout>
       <h1 className='text-xl'>About Me</h1>
 
-      <StaticImage alt='' src='' placeholder='blurred'></StaticImage>
+      <StaticImage
+        alt='barista making pour over coffee in a coffee shop'
+        src='../images/pourover.jpeg'
+        placeholder='blurred'
+        className='rounded-xl'
+      />
     </Layout>
   );
 };
 
 export default AboutPage;
 
-export const Head = () => <title>About Me</title>;
+export const Head = () => <Seo pageTitle='About Me' />;
