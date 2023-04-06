610d34bfe45dc48caa49af87750cb67dba634790
Zak Mayfield <zakmayfield@gmail.com>
2023-04-06 13:33:55 -0400

    typescript adjustments & new query

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,10 +1,22 @@
 import * as React from 'react';
-import { Link, PageProps } from 'gatsby';
+import { Link, PageProps, graphql } from 'gatsby';
 import Layout from '../components/layout';
 import { StaticImage } from 'gatsby-plugin-image';
 import Seo from '../components/seo';
 
-const BlogPage: React.FC<PageProps> = () => {
+type NameType = {
+  name: string
+}
+
+type BlogDataType = PageProps & {
+  data: {
+    allFile?: {
+      nodes: NameType[];
+    };
+  };
+}
+
+const BlogPage = ({ data }: BlogDataType) => {
   return (
     <Layout>
       <h1 className='text-xl'>Blog</h1>
@@ -18,10 +30,28 @@ const BlogPage: React.FC<PageProps> = () => {
 
       <div className='mt-5'>
         <h2 className='text-xl'>Coffee Havens Latest Blogs</h2>
+
+        <div>
+          <ul>
+            {data.allFile?.nodes.map((node: any) => (
+              <li key={node.name}>{node.name}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </Layout>
   );
 };
+
+export const query = graphql`
+  query AllFile {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
 
 export default BlogPage;
 
